df7f16e07a24637d3a75488e7aa3e8fe6b7ced07
Ruben Barragan <ruben_gbp_91@hotmail.com>
2016-04-10 17:02:01 -0500

    add functions to HelloServer

--- ServerCorba/src/HelloServer/HelloServer.java
+++ ServerCorba/src/HelloServer/HelloServer.java
@@ -14,6 +14,17 @@ package HelloServer;
 // HelloServer.java
 // Copyright and License 
 import commun.*;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.omg.CosNaming.*;
 import org.omg.CosNaming.NamingContextPackage.*;
 import org.omg.CORBA.*;
@@ -21,6 +32,8 @@ import org.omg.PortableServer.*;
 import org.omg.PortableServer.POA;
 
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class HelloImpl extends CORBA_InterfacePOA {
 
@@ -30,34 +43,225 @@ class HelloImpl extends CORBA_InterfacePOA {
         orb = orb_val;
     }
 
-    // implement sayHello() method
-    public String sayHello(String s) {
+    public String sayHello() {
+        return "Hello, a wii!";
+    }
+
+    public String selectAll() {
+
+        String returnedQuery = "";
+
+        ConnectBD cbd = new ConnectBD();
+        try {
+            Connection con = cbd.connectBD();
+
+            //stmt is the statement's object. It's used to create statements or queries.
+            Statement stmt = con.createStatement();
+
+            //devices is the table's name.
+            ResultSet rs = stmt.executeQuery("select * from devices");
+
+            while (rs.next()) {
+                returnedQuery += rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + " " + rs.getString(4) + " " + rs.getString(5);
+                //System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + " " + rs.getInt(4));
+            }
+
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(HelloImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return returnedQuery;
+    }
+
+    //Only local computing.
+    public int selectRow(String id, String otherDate) {
+        // 0 = no existe 1 = si existe y hacer cambio 2= si existe y no hacer cambio
+        int returnedQuery = 0;
+        String theDate = "";
+
+        ConnectBD cbd = new ConnectBD();
+        try {
+            Connection con = cbd.connectBD();
+            //stmt is the statement's object. It's used to create statements or queries.
+            Statement stmt = con.createStatement();
+
+            //devices is the table's name.
+            ResultSet rs = stmt.executeQuery("select * from devices where id_bluetooth = '" + id + "'");
+
+            while (rs.next()) {
+                theDate = rs.getString(5);
+                returnedQuery = compareDates(theDate, otherDate);
+            }
+
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(HelloImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return returnedQuery;
+    }
+
+    public int compareDates(String theDate, String otherDate) {
+        int result = -1;
+        Timestamp timestamp2 = null;
+        Timestamp timestamp = null;
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            Date parsedDate = dateFormat.parse(theDate);
+            timestamp = new java.sql.Timestamp(parsedDate.getTime());
+
+            Date parsedDate2 = dateFormat.parse(otherDate);
+            timestamp2 = new java.sql.Timestamp(parsedDate2.getTime());
+
+            if (timestamp.before(timestamp2)) {
+                result = 1;
+            } else {
+                result = 2;
+            }
+
+        } catch (Exception e) {//this generic but you can control another types of exception
+            System.out.println(e.toString());
+        }
+        return result;
+    }
+
+    public String insertRow(String ibt, String name, String lugar, String datetime) {
+
+        String returnedQuery = "Cosa";
+
+        ConnectBD cbd = new ConnectBD();
+        try {
+            Connection con = cbd.connectBD();
+
+            //stmt is the statement's object. It's used to create statements or queries.
+            Statement stmt = con.createStatement();
+
+            //devices is the table's name.
+            stmt.executeUpdate("INSERT INTO `locator`.`devices` (`id_bluetooth`, `name`, `lugar`, `datetime`) VALUES ('" + ibt + "', '" + name + "', '" + lugar + "', '" + datetime + "')");
+
+            System.out.println("All right");
+
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(HelloImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return returnedQuery;
+    }
+
+    public String updateRow(String ibt, String lugar, String datetime) {
+
+        String returnedQuery = "";
+
+        ConnectBD cbd = new ConnectBD();
+        try {
+            Connection con = cbd.connectBD();
+
+            //stmt is the statement's object. It's used to create statements or queries.
+            Statement stmt = con.createStatement();
+
+            stmt.executeUpdate("UPDATE `devices` SET `lugar`='" + lugar + "',`datetime`='" + datetime + "' WHERE id_bluetooth='" + ibt + "'");
+
+            System.out.println("All right");
+
+//            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(HelloImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return returnedQuery;
+    }
+
+    public void recoveryBD(String ibt, String name, String lugar, String datetime) {
+        int result = exists_idBT(ibt, datetime);
+        if (result== 1) {
+            updateRow(ibt, lugar, datetime);
+        } else if (result == 0){
+            insertRow(ibt, name, lugar, datetime);
+        }
+    }
+
+    //Only local computing.
+    public int exists_idBT(String id, String otherDate) {
+        int s = selectRow(id, otherDate);
         return s;
     }
 
-    // implement shutdown() method
-    public void shutdown() {
-        orb.shutdown(false);
+    //Only local computing.
+    public static void testBDConnection() {
+        ConnectBD cbd = new ConnectBD();
+        Connection con = null;
+
+        con = cbd.connectBD();
+        while (con == null) {
+            con = cbd.connectBD();
+        }
+
     }
 
-    @Override
-    public String selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String insertRow() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String updateRow() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean _isEmpty = false;
+
+        ConnectBD cbd = new ConnectBD();
+        Connection con = null;
+        try {
+            con = cbd.connectBD();
+
+            //stmt is the statement's object. It's used to create statements or queries.
+            Statement stmt = con.createStatement();
+
+            //devices is the table's name.
+            ResultSet rs = stmt.executeQuery("SELECT * FROM `devices`");
+
+            if (!rs.isBeforeFirst()) {
+                System.out.println("No data");
+                _isEmpty = true;
+            }
+
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(HelloImpl.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        return _isEmpty;
+    }
+
+    public void giveMeYourBD() {
+        ResultSet rs = null;
+
+        ConnectBD cbd = new ConnectBD();
+        try {
+            Connection con = cbd.connectBD();
+
+            //stmt is the statement's object. It's used to create statements or queries.
+            Statement stmt = con.createStatement();
+
+            //devices is the table's name.
+            rs = stmt.executeQuery("select * from devices");
+
+            Registry registry;
+            try {
+                registry = LocateRegistry.getRegistry(externalIP, 1099);
+                RMI_Interface stub = (RMI_Interface) registry.lookup("rmi://" + externalIP + ":1099/RMI_Interface");
+
+                while (rs.next()) {
+                    stub.recoveryBD(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+                }
+
+            } catch (RemoteException ex) {
+                Logger.getLogger(ServerRMI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NotBoundException ex) {
+                Logger.getLogger(ServerRMI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(ServerRMI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ServerRMI.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
 
